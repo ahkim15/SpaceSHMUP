1ca5f717a582e99a75a4b42d937ca9318937bdde
Alison <alisonkim@nbp-84-112.nbp.ruw.rutgers.edu>
2016-07-12 16:58:00 -0400

    Making the Hero Ship

---
+++ Space SHMUP Prototype/Assets/__Scripts/Hero.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hero : MonoBehaviour {
+	static public Hero S;	// singleton
+
+	// these fields control the movement of the ship
+	public float speed = 30;
+	public float rollMult = -45;
+	public float pitchMult = 30;
+
+	// ship status information
+	public float shieldLevel = 1;
+
+	public bool ________________;
+
+	public Bounds bounds;
+
+	void Awake () {
+		S = this;	// set the singleton
+		bounds = Utils.CombineBoundsOfChildren (this.gameObject);
+	}
+
+	void Update () {
+		// pull in information from the Input class
+		float xAxis = Input.GetAxis ("Horizontal");			// 1. line 23-24 see pg 492
+		float yAxis = Input.GetAxis ("Vertical");
+
+		// change transform.position based on the axes
+		Vector3 pos = transform.position;
+		pos.x += xAxis * speed * Time.deltaTime;
+		pos.y += yAxis * speed * Time.deltaTime;
+		transform.position = pos;
+
+		bounds.center = transform.position;		// 1. see pg 504
+
+		// keep the ship constrained to the screen bounds
+		Vector3 off = Utils.ScreenBoundsCheck (bounds, BoundsTest.onScreen);	// 2. see pg 504
+		if (off != Vector3.zero) {				// 3. see pg 504
+			pos -= off;
+			transform.position = pos;
+		}
+
+		// rotate the ship to make it feel more dynamic		// 2. see pg 492
+		transform.rotation = Quaternion.Euler (yAxis * pitchMult, xAxis * rollMult, 0);
+	}
+}
---
+++ Space SHMUP Prototype/Assets/__Scripts/Shield.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shield : MonoBehaviour {
+	public float rotationsPerSecond = 0.1f;
+	public bool ___________________;
+	public int levelShown = 0;
+
+	void Update () {
+		// read the current shield level from the Hero Singleton
+		int currLevel = Mathf.FloorToInt (Hero.S.shieldLevel);				// 1. see pg 495
+		// if this is different from levelShown ...
+		if (levelShown != currLevel) {
+			levelShown = currLevel;
+			Material mat = this.renderer.material;
+			// adjust the texture offset to show different shield level
+			mat.mainTextureOffset = new Vector2 (0.2f * levelShown, 0);		// 2. see pg 495
+		}
+		//rotate the shield a bit every second
+		float rZ = (rotationsPerSecond * Time.time * 360) % 360f;			// 3. see pg 495
+		transform.rotation = Quaternion.Euler (0, 0, rZ);
+	}
+}
