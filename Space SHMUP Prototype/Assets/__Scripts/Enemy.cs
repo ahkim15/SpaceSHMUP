9ae43ae77726f58b05369a724a9c4402b0e6e6e3
Alison <alisonkim@nbp-89-221.nbp.ruw.rutgers.edu>
2016-07-12 20:26:55 -0400

    Adding & Spawning Enemies
    setting tags, laters, and physics

---
+++ Space SHMUP Prototype/Assets/__Scripts/Enemy.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+ 	public float speed = 10f;		// the speed in m/s
+	public float fireRate = 0.3f;	// seconds/shot (unused)
+	public float health = 10;
+	public int score = 100;			// points earned for destroying this
+
+	public bool ________________;
+
+	public Bounds bounds;			// the bounds of this and its children
+	public Vector3 boundsCenterOffset;	// dist of bounds.center from position
+
+	void Awake () {
+		InvokeRepeating ("CheckOffscreen", 0f, 2f);
+	}
+
+	// update is called once per frames
+	void Update () {
+		Move ();
+	}
+
+	public virtual void Move() {
+		Vector3 tempPos = pos;
+		tempPos.y -= speed * Time.deltaTime;
+		pos = tempPos;
+	}
+
+	// this is a property: a method that acts like a field
+	public Vector3 pos {
+		get {
+			return (this.transform.position);
+		}
+		set {
+			this.transform.position = value;
+		}
+	}
+
+	void CheckOffscreen () {
+		// if bounds are sill their default value ...
+		if (bounds.size == Vector3.zero) {
+			// then set them
+			bounds = Utils.CombineBoundsOfChildren (this.gameObject);
+			// also find the diff between bounds.center & transform.position
+			boundsCenterOffset = bounds.center - transform.position;
+		}
+
+		// every time, update the bounds to the current position
+		bounds.center = transform.position + boundsCenterOffset;
+		// check to see whether the bounds are completely offscreen
+		Vector3 off = Utils.ScreenBoundsCheck (bounds, BoundsTest.offScreen);
+		if (off != Vector3.zero) {
+			// if this enemy has gone off the bottom edge of the screen
+			if (off.y < 0) {
+				// then destroy it
+				Destroy (this.gameObject);
+			}
+		}
+	}
+
+}
---
+++ Space SHMUP Prototype/Assets/__Scripts/Main.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Main : MonoBehaviour {
+	static public Main S;
+
+	public GameObject [] prefabEnemies;
+	public float enemySpawnPerSecond = 0.5f;	// # enemies/second
+	public float enemySpawnPadding = 1.5f;		// padding for position
+
+	public bool _______________________;
+
+	public float enemySpawnRate;			// delay between enemy spawns
+
+	void Awake () {
+		S = this;
+		// set Utils.camBounds
+		Utils.SetCameraBounds (this.camera);
+		// 0.5 enemies/second = enemySpawnRate of 2
+		enemySpawnRate = 1f / enemySpawnPerSecond;					// 1. see pg 510
+		// invoke call SpawnEnemy() once after a 2 second delay
+		Invoke ("SpawnEnemy", enemySpawnRate);						// 2. see pg 510
+	}
+
+	public void SpawnEnemy () {
+		// pick a random Enemy prefab to instantiate
+		int ndx = Random.Range (0, prefabEnemies.Length);
+		GameObject go = Instantiate (prefabEnemies [ndx]) as GameObject;
+		// position the enemy above the screen with a random x position
+		Vector3 pos = Vector3.zero;
+		float xMin = Utils.camBounds.min.x + enemySpawnPadding;
+		float xMax = Utils.camBounds.max.x - enemySpawnPadding;
+		pos.x = Random.Range (xMin, xMax);
+		pos.y = Utils.camBounds.max.y + enemySpawnPadding;
+		go.transform.position = pos;
+		// call SpawnEnemy() again in a couple of seconds
+		Invoke ("SpawnEnemy", enemySpawnRate);						// 3. see pg 510
+	}
+
+}
